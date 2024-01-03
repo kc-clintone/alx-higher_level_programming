40204d1d4e1677418bb442644384d956da1d45af
kc-clintone <kayseeclintone@gmail.com>
2024-01-03 22:07:58 +0000

    mod3

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 
-print(str[39:-63]+str[107:-18])
+print(str[39:-63]+str[107:-18]+str[6:])
