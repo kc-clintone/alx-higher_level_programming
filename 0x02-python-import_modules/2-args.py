64f2451c823b054193ede89a4238b3a27cab5b15
kc-clintone <kayseeclintone@gmail.com>
2024-01-04 21:24:13 +0000

    another file now

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    num_args = len(argv) - 1
+
+    print("{} argument{}{}:".format(num_args, 's' if num_args != 1 else '', '' if num_args == 0 else ':'))
+    for i, arg in enumerate(argv[1:], start=1):
+        print("{}: {}".format(i, arg))
+
