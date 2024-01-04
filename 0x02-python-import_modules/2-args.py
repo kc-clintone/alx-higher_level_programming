7e3e72e79c42e8c1643feb1af4c4d856ef38efb5
kc-clintone <kayseeclintone@gmail.com>
2024-01-04 21:25:52 +0000

    editing

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-from sys import argv
 
 if __name__ == "__main__":
-    num_args = len(argv) - 1
+    """Print the number of 
 
-    print("{} argument{}{}:".format(num_args, 's' if num_args != 1 else '', '' if num_args == 0 else ':'))
-    for i, arg in enumerate(argv[1:], start=1):
-        print("{}: {}".format(i, arg))
+    import sys
 
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
