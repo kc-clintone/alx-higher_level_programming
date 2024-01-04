1cf3f968cc13fbb23fd26f2b32c5a8711ef9d6eb
kc-clintone <kayseeclintone@gmail.com>
2024-01-04 21:40:03 +0000

    editting..

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,3 +3,15 @@ if __name__ == "__main__":
     module_names = [name for name in dir(hidden_4) if not name.startswith("__")]
     for name in sorted(module_names):
         print(name)
+
+
+
+
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import hidden_4
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
