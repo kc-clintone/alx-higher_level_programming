8615773a35ce270acf26594afe24488225804ad6
kc-clintone <kayseeclintone@gmail.com>
2024-01-07 19:57:37 +0000

    a script that adds the numer of arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,13 @@
+#!/usr/bin/python3
+
+def sum_args(*args):
+    try:
+        total = sum(int(arg) for arg in args)
+        print(total)
+    except ValueError:
+        print("Error")
+
+if __name__ == "__main__":
+    import sys
+    sum_args(*sys.argv[1:])
 
