6039c330cb13696524e66a0d76837769fd1e8fde
kc-clintone <kayseeclintone@gmail.com>
2024-01-14 03:39:36 +0000

    back to see

---
+++ 0x03-python-data_structures/13-palindrome.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct listint_s
+{
+int n;
+struct listint_s *next;
+};
+typedef struct listint_s listint_t;
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: Pointer to the head of the linked list.
+ * Return: 1 if it is a palindrome, 0 if it is not.
+ */
+int is_palindrome(listint_t **head)
+{
+if (*head == NULL || (*head)->next == NULL)
+return (1)
+
+listint_t *slow = *head;
+listint_t *fast = *head;
+listint_t *prev = NULL;
+listint_t *temp;
+int result = is_palindrome(&head);
+while (fast != NULL && fast->next != NULL)
+{
+fast = fast->next->next;
+temp = slow->next;
+slow->next = prev;
+prev = slow;
+slow = temp;
+}
+if (fast != NULL)
+slow = slow->next;
+
+while (slow != NULL)
+{
+if (slow->n != prev->n)
+return (0);
+
+slow = slow->next;
+prev = prev->next;
+}
+
+return (1);
+}
