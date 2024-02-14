613f7caa9012ffb95d1a44f3f61e43d74d76f7e7
kc-clintone <kayseeclintone@gmail.com>
2024-02-14 20:41:52 +0000

    task >>> 002

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Now this is a module...more later"""
+
+class Square:
+    """We add stuff...to the class"""
+    def __init__(self, size=0):
+        """Let's see what we've added
+        args:
+            size: size of the square
+        raises:
+            TypeError: if size != integer
+            ValueError: if size < zero
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
