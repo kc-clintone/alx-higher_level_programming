355f2a1ffda6d206945bb45454d415e71daa5cc3
kc-clintone <kayseeclintone@gmail.com>
2024-02-14 20:35:50 +0000

    task >>> 001

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Square class continued... """
+
+class Square:
+    """This part defines a square"""
+    def __init__(self, size):
+        """Initializing the class"""
+        self.__size = size
