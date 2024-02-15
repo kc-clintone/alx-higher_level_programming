cb639bf6e43f1316e07c01dbf4b69217f517f0b0
kc-clintone <kayseeclintone@gmail.com>
2024-02-15 20:52:24 +0000

    task >>> 001 changes >>01<<

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,19 +2,9 @@
 """Let's engineer a class that deals with rectangles"""
 class Rectangle:
     """Well now it does something"""
-    def __init__(self, width=0, height=0):
-        self.width = width
+    def __init__(self, height=0, width=0):
         self.height = height
-    @property
-    def width(self):
-        return self.__width
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
+        self.width = width
     @property
     def height(self):
         return self.__height
@@ -25,3 +15,19 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+    @property
+    def width(self):
+        return self.__width
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+    def area(self):
+        return (self.__height * self.__width)
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return (0)
+        return ((self.__height * 2) + (self.__width * 2))
