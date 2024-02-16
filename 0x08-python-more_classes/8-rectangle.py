e63ed999dfcb5146eaa086edc6a51d6889d844d3
kc-clintone <kayseeclintone@gmail.com>
2024-02-16 21:27:32 +0000

    task >>> 008 mod [01]

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -2,14 +2,16 @@
 """Let's engineer a class that deals with rectangles"""
 class Rectangle:
     """Well now it does something"""
-    def __init__(self, height=0, width=0):
+    number_of_instances = 0
+    print_symbol = "#"
+    def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
         Rectangle.number_of_instances += 1
     @property
     def height(self):
         return self.__height
-            @height.setter
+    @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
@@ -33,12 +35,15 @@ class Rectangle:
             return (0)
         return ((self.__height * 2) + (self.__width * 2))
     def __str__(self) -> str:
-        if self.__height == 0 or self.__width == 0:
+        if self.__width == 0 or self.__height == 0:
             return ("")
         rect = ""
         for c in range(self.__height):
             for r in range(self.__width):
-                rect += "#"
+                try:
+                    rect += str(self.print_symbol)
+                except Exception:
+                    rect += type(self).print_symbol
             if c < self.__height - 1:
                 rect += "\n"
         return (rect)
@@ -46,7 +51,7 @@ class Rectangle:
         return "Rectangle({:d}, {:d})".format(self.__height, self.__width)
     def __del__(self):
         """prompt for showing deleted objects"""
-        print("Success. Deleted")
+        print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
