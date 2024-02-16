2b3bc94c17ed29c75aefc2947fdcd809eee7bb9f
kc-clintone <kayseeclintone@gmail.com>
2024-02-16 21:07:51 +0000

    task >>> 007 changes >>03<<

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -2,7 +2,8 @@
 """Let's engineer a class that deals with rectangles"""
 class Rectangle:
     """Well now it does something"""
-    number_of_instances=0
+    number_of_instances = 0
+    print_symbol = "#"
     def __init__(self, height=0, width=0):
         self.height = height
         self.width = width
@@ -34,12 +35,15 @@ class Rectangle:
             return (0)
         return ((self.__height * 2) + (self.__width * 2))
     def __str__(self) -> str:
-        if self.__height == 0 or self.__width == 0:
+        if self.__width == 0 or self.__height == 0:
             return ("")
         rect = ""
         for c in range(self.__height):
             for r in range(self.__width):
-                rect += "#"
+                try:
+                    rect += str(self.print_symbol)
+                except Exception:
+                    rect += type(self).print_symbol
             if c < self.__height - 1:
                 rect += "\n"
         return (rect)
