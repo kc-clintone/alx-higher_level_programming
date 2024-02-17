b31cab147ef207760db5a36a2f5f073409521654
kc-clintone <kayseeclintone@gmail.com>
2024-02-17 11:03:18 +0000

    almost a circle >>> task 013

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -30,6 +30,23 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
+    def update(self, *args, **kwargs):
+        """
+        Assigning arguments to attributes based on their type, more args.
+
+        Args:
+            *args: No-keyword arguments in the order (id, size, x, y).
+            **kwargs: Key-worded arguments representing attribute assignments.
+        """
+        if args:
+            self.id = args[0] if len(args) >= 1 else self.id
+            self.size = args[1] if len(args) >= 2 else self.size
+            self.x = args[2] if len(args) >= 3 else self.x
+            self.y = args[3] if len(args) >= 4 else self.y
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
     def __str__(self):
         """Override the __str__ method."""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
