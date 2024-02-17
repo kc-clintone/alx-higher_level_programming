ef3cb90310c1ae03c15821bdb2a63d3284f7d628
kc-clintone <kayseeclintone@gmail.com>
2024-02-17 09:34:46 +0000

    almost a circle >>> task 010

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -89,23 +89,23 @@ class Rectangle(Base):
             print()
         for _ in range(self.__height):
             print(" " * self.__x + "#" * self.__width)
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """
-        Assigning arguments to attributes in the specified order. Dunno why.
+        Assigning arguments to attributes based on their type, more updates yet.
 
         Args:
             *args: No-keyword arguments in the order (id, width, height, x, y).
+            **kwargs: Key-worded arguments representing attribute assignments.
         """
-        if len(args) >= 1:
-            self.id = args[0]
-        if len(args) >= 2:
-            self.width = args[1]
-        if len(args) >= 3:
-            self.height = args[2]
-        if len(args) >= 4:
-            self.x = args[3]
-        if len(args) >= 5:
-            self.y = args[4]
+        if args:
+            self.id = args[0] if len(args) >= 1 else self.id
+            self.width = args[1] if len(args) >= 2 else self.width
+            self.height = args[2] if len(args) >= 3 else self.height
+            self.x = args[3] if len(args) >= 4 else self.x
+            self.y = args[4] if len(args) >= 5 else self.y
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
     def __str__(self):
         """Overriding the __str__ method and doing some weired stuff"""
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
