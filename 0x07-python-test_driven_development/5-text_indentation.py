123d6ca7aa2f3ed201acc8400c9c26be0c5a3214
kc-clintone <kayseeclintone@gmail.com>
2024-02-18 19:47:17 +0000

    python-test_driven_development >>>mod [01]

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -4,13 +4,13 @@ This module contains a function that prints a text with 2
 new lines after each of these characters: ., ? and :
 """
 def text_indentation(text):
+    """lets see some indents"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
     count = 0
     while count < len(text) and text[count] == " ":
         count = count + 1
-
     while count < len(text):
         print(text[count], end="")
         if text[count] == "\n" or text[count] in ".?:":
@@ -21,3 +21,4 @@ def text_indentation(text):
                 count = count + 1
             continue
         count = count + 1
+
