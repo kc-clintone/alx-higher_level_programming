20a466ed6b249692bb647ff17685d8a21acda361
kc-clintone <kayseeclintone@gmail.com>
2024-02-21 13:04:27 +0000

    little mod

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a Student main class"""
 
+
 class Student:
     """This represent a student class"""
     def __init__(self, first_name, last_name, age):
@@ -8,12 +9,14 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self, attrs=None):
         """Refrences a dictionary representation of the Student object"""
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
+
     def reload_from_json(self, json):
         """Replaces all attributes of the Student object/class"""
         for k, v in json.items():
