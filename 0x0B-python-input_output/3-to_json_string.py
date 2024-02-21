83a0ee47c124d60c7c725ff729e7de03bb093b7d
kc-clintone <kayseeclintone@gmail.com>
2024-02-21 06:04:35 +0000

    0x0B-python-input_output >>> task [03]

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module contains a string-to-JSON convertion function"""
+
+import json
+
+def to_json_string(my_obj):
+    """Thie function returns the JSON version of a string object"""
+    return json.dumps(my_obj)
