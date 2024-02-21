5d686cb01f9be1ff5a9f05ed720108eebb4d94a2
kc-clintone <kayseeclintone@gmail.com>
2024-02-21 06:15:33 +0000

    0x0B-python-input_output >>> task [09]

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module defines a Student main class"""
+
+class Student:
+    """This represent a student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializing a new Student object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    def to_json(self):
+        """Refrences the dict representation of the Student"""
+        return self.__dict__
