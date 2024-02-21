83fd820a516b45e82fc257df2f13a3b0225f2375
kc-clintone <kayseeclintone@gmail.com>
2024-02-21 06:23:38 +0000

    0x0B-python-input_output >>> task [12]

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module defines the famouse Pascal's Triangle function"""
+
+def pascal_triangle(n):
+    """This function represents Pascal's Triangle of size n"""
+    if n <= 0:
+        return []
+    tri = [[1]]
+    while len(tri) != n:
+        trio = tri[-1]
+        temp = [1]
+        for index in range(len(trio) - 1):
+            temp.append(trio[index] + trio[index + 1])
+        temp.append(1)
+        tri.append(temp)
+    return tri
