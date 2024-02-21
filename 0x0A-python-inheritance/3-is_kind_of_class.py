266008688506028337fb30505d0d4e0361c6e8eb
kc-clintone <kayseeclintone@gmail.com>
2024-02-21 12:08:19 +0000

    little mod

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Checks if a class is inherited or not"""
 
+
 def is_kind_of_class(obj, a_class):
     """Returns true if a class is inherited or the class that is inherited
     from
