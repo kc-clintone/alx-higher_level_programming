1624b2f59a7769c7b3ad65b3beeb04200e28fc55
kc-clintone <kayseeclintone@gmail.com>
2024-02-21 13:17:36 +0000

    little mod

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,7 +3,12 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """This function inserts text after each line containing a given string in a file"""
+
+    """This function inserts text after each line containing a given string
+    in a file
+    """
+
+
     the_txt = ""
     with open(filename) as r:
         for i in r:
