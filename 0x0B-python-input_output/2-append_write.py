21e6a21119674f0b7ef89cd5e7c7572d543ab577
kc-clintone <kayseeclintone@gmail.com>
2024-02-21 12:58:59 +0000

    little mod

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module contains a file-appending function."""
 
+
 def append_write(filename="", text=""):
     """This function appends content to the end of a UTF8 text file"""
     with open(filename, "a", encoding="utf-8") as f:
