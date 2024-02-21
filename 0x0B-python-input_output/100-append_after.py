960be4c370bab0a664b22ca1707b1c31bf6fb170
kc-clintone <kayseeclintone@gmail.com>
2024-02-21 13:06:07 +0000

    little mod

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a text/file insertion function"""
 
+
 def append_after(filename="", search_string="", new_string=""):
     """This function inserts text after each line containing a given string in a file"""
     the_txt = ""
