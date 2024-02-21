111d09e533098487d8d6639b90d13da2b17a2844
kc-clintone <kayseeclintone@gmail.com>
2024-02-21 12:46:57 +0000

    little mod

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -4,9 +4,13 @@
 
 class MyInt(int):
     """This class invert int operators == and !="""
+
     def __eq__(self, value):
         """Overrides the == opeartor with the != behavior"""
+
         return self.real != value
+
     def __ne__(self, value):
         """Overrides the != operator with the == behavior"""
+
         return self.real == value
