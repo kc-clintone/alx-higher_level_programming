ee4701ef51393f7d7039af5fff43e179d26233aa
kc-clintone <kayseeclintone@gmail.com>
2024-02-21 06:09:40 +0000

    0x0B-python-input_output >>> task [06]

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains a JSON file-reading function"""
+
+import json
+
+def load_from_json_file(filename):
+    """This function creates a Python object from reading a given JSON file"""
+    with open(filename) as f:
+        return json.load(f)
