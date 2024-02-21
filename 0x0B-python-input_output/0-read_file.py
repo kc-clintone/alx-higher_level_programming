0a1b7661ef1976a5ad5ccc726ac2a0fd69b87ea1
kc-clintone <kayseeclintone@gmail.com>
2024-02-21 12:58:35 +0000

    little mod

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module has a funtion for text/file reading capability"""
 
+
 def read_file(filename=""):
     """This function prints contents of a UTF8 text file"""
     with open(filename, encoding="utf-8") as f:
