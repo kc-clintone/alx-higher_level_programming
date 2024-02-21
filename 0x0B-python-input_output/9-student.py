b3af5b92f2ea0c72e4fe6791060e6cd76df605a5
kc-clintone <kayseeclintone@gmail.com>
2024-02-21 13:03:10 +0000

    little mod

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a Student main class"""
 
+
 class Student:
     """This represent a student class"""
 
@@ -9,6 +10,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self):
         """Refrences the dict representation of the Student"""
         return self.__dict__
