da5355d5ea269adb813b44c58b59f19dcb5630f3
kc-clintone <kayseeclintone@gmail.com>
2024-02-21 06:12:53 +0000

    0x0B-python-input_output >>> task [08]

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""This module contains a Python class-to-JSON function"""
+
+def class_to_json(obj):
+    """This function returns a dictionary version of a simple data structure"""
+    return obj.__dict__
