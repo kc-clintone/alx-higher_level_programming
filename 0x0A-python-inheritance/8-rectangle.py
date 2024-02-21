7016bafc85c08c119d9cb8e44ee705f8ccedaa75
kc-clintone <kayseeclintone@gmail.com>
2024-02-21 12:35:32 +0000

    little mod

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,6 +4,7 @@
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """Define rectangle from the class BaseGeometry"""
     def __init__(self, width, height):
