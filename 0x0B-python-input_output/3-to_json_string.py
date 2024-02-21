44ad73fe1ac6f876e10da12eb191e032fa6842d9
kc-clintone <kayseeclintone@gmail.com>
2024-02-21 12:59:20 +0000

    little mod

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """This module contains a string-to-JSON convertion function"""
 
+
 import json
+
 
 def to_json_string(my_obj):
     """Thie function returns the JSON version of a string object"""
