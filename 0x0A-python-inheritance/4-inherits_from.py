4da70df95d919e7c7a70a5363874fe76e76eae4d
kc-clintone <kayseeclintone@gmail.com>
2024-02-21 12:08:43 +0000

    little mod

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """This module checks if object is an instance of a class or not"""
+
+
 def inherits_from(obj, a_class):
     """Returns true if object is an instance of a class that is inherited
     from the specified class; else returns False
