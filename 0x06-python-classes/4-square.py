16cae3e0251d5d6e5663829452012327355804e0
kc-clintone <kayseeclintone@gmail.com>
2024-02-26 21:08:38 +0000

    0x06-python-classes >>> corrections

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Still a continuation, just expanding the class"""
 
+
 class Square:
     """You already know what this is"""
     def __init__(self, size=0):
@@ -16,10 +17,12 @@ class Square:
         if size < 0:
             raise ValueError('size must be >= 0')
         self.__size = size
+
     @property
     def size(self):
         """Well, this function gets the size of the square"""
         return self.__size
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -27,6 +30,7 @@ class Square:
         if value < 0:
             raise ValueError('size must be >= 0')
         self.__size = value
+
     def area(self):
         """
         Now, this function calculates the area of the square,
