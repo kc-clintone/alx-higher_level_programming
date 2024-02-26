46b840ecfcadfc86c48a0f655996fa9e767d111c
kc-clintone <kayseeclintone@gmail.com>
2024-02-26 22:16:45 +0000

    0x08-python-more_classes >>> corrections

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 """Let's engineer a class that deals with rectangles"""
+
+
 class Rectangle:
     """Well now it does something"""
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
+
     @property
     def height(self):
         return self.__height
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -15,9 +19,11 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
     @property
     def width(self):
         return self.__width
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -25,12 +31,15 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
     def area(self):
         return (self.__height * self.__width)
+
     def perimeter(self):
         if self.__height == 0 or self.__width == 0:
             return (0)
         return ((self.__height * 2) + (self.__width * 2))
+
     def __str__(self) -> str:
         if self.__height == 0 or self.__width == 0:
             return ("")
@@ -41,5 +50,6 @@ class Rectangle:
             if c < self.__height - 1:
                 rect += "\n"
         return (rect)
+
     def __repr__(self):
         return "Rectangle({:d}, {:d})".format(self.__height, self.__width)
