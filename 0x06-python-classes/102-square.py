ed2ee906057683fb2c79f1b7a8eec8c3139b3d63
kc-clintone <kayseeclintone@gmail.com>
2024-02-26 21:35:36 +0000

    0x06-python-classes >>> corrections

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """defining the square"""
+
+    def __init__(self, size=0):
+        """Instanciate the Square
+        Args: size: length
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """"Props
+        Raises:
+            TypeError: if size != int
+            ValueErrorr: if size < 0
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """Get area of Square
+        Returns: Size squared
+        """
+        return self.__size * self.__size
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __eq__(self, other):
+        return self.area() == other.area()
