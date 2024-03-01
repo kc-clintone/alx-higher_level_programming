98594bd70659ad7777675baa64814efcbf3e5f64
kc-clintone <kayseeclintone@gmail.com>
2024-03-01 07:47:38 +0000

    0x03-python-data_structures >>> corrections

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,38 +1,66 @@
 #include "lists.h"
+
 /**
- * is_palindrome - Checks if a singly linked list is a palindrome.
- * @head: Pointer to the head of the linked list.
- * Return: 1 if it is a palindrome, 0 if it is not.
- */
+ * reverse_listint - This function reverses a linked list.
+ * @head: Pointer to first node.
+ * Return: Pointer to first node in new list.
+*/
+void reverse_listint(listint_t **head)
+{
+listint_t *prev = NULL;
+listint_t *next = NULL;
+listint_t *cur = *head;
+
+while (cur)
+{
+next = cur->next;
+cur->next = prev;
+prev = cur;
+cur = next;
+}
+*head = prev;
+}
+
+/**
+ * is_palindrome - This right here checks if a linked-list is a
+ * palindrome
+ * @head: A double pointer to the linked-list.
+ * Return: 1 if true, 0 Otherwise.
+*/
 int is_palindrome(listint_t **head)
 {
+listint_t *x = *head, *y = *head;
+listint_t *tmp = *head, *dbl = NULL;
+
 if (*head == NULL || (*head)->next == NULL)
-return (1)
-
-listint_t *slow = *head;
-listint_t *fast = *head;
-listint_t *prev = NULL;
-listint_t *temp;
-int result = is_palindrome(&head);
-while (fast != NULL && fast->next != NULL)
-{
-fast = fast->next->next;
-temp = slow->next;
-slow->next = prev;
-prev = slow;
-slow = temp;
-}
-if (fast != NULL)
-slow = slow->next;
-
-while (slow != NULL)
-{
-if (slow->n != prev->n)
-return (0);
-
-slow = slow->next;
-prev = prev->next;
-}
-
 return (1);
+while (1)
+{
+y = y->next->next;
+if (!y)
+{
+dbl = x->next;
+break;
+}
+if (!y->next)
+{
+dbl = x->next->next;
+break;
+}
+x = x->next;
+}
+reverse_listint(&dbl);
+while (dbl && tmp)
+{
+if (tmp->n == dbl->n)
+{
+dbl = dbl->next;
+tmp = tmp->next;
+}
+else
+return (0);
+}
+if (!dbl)
+return (1);
+return (0);
 }
