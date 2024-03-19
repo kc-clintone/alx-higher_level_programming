36d05c7e2d0f5d4991864f9d864fc8969d4b3693
kc-clintone <kayseeclintone@gmail.com>
2024-03-19 19:47:57 +0000

    0x0D-SQL_introduction >>> corrections

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -2,4 +2,6 @@
 -- Results should display both the score and the name (in this order)
 -- Records should be ordered by score (top first)
 
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
