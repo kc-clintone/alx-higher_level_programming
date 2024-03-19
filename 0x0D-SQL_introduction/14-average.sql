ec8a2a97d3fd623f029f717ae0a95c040aa7b2c5
kc-clintone <kayseeclintone@gmail.com>
2024-03-19 19:49:43 +0000

    0x0D-SQL_introduction >>> corrections

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,4 +1,6 @@
 -- computes the score average of all records in the table second_table of the database hbtn_0c_0 in  ySQL server.
 -- The result column name should be average
 
-SELECT AVG(score) AS average FROM second_table;
+SELECT AVG(score)
+AS average
+FROM second_table;
