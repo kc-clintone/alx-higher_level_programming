65e033cc50c588191bf54afad3a643c2ecdcf71e
kc-clintone <kayseeclintone@gmail.com>
2024-03-19 19:53:51 +0000

    0x0D-SQL_introduction >>> corrections

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,4 +1,10 @@
 -- Displays the top 3 of cities temp 
 -- uring July & august ordered by temp (descending)
 
-SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month=7 OR month=8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value)
+AS avg_temp
+FROM temperatures
+WHERE month=7
+OR month=8
+GROUP BY city
+ORDER BY avg_temp DESC LIMIT 3;
