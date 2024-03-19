ae12a90bc100bf1a4d9fe6bea61a987be38ed727
kc-clintone <kayseeclintone@gmail.com>
2024-03-19 18:55:09 +0000

    0x0D-SQL_introduction >>> tasks [6]

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,4 @@
+-- Lists all rows of the first_table table from the hbtn_0c_0 database in MySQL server
+-- All fields should be printed                                               -- The database name will be passed as an argument of the mysql command
+
+SELECT * FROM first_table;
