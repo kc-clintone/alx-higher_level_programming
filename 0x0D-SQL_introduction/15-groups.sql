d06e6fca117c45428fcb8a5266c6cd6b75ad9089
kc-clintone <kayseeclintone@gmail.com>
2024-03-19 19:50:10 +0000

    0x0D-SQL_introduction >>> corrections

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -4,4 +4,7 @@
 --      he number of records for this score with the label number
 -- The list should be sorted by the number of records (descending)
 
-SELECT score, COUNT(*) as number FROM second_table GROUP BY score ORDER BY number DESC;
+SELECT score, COUNT(*) as number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC;
