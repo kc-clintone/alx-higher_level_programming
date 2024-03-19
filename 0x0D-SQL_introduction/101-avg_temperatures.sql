5c142a0f29fcd407e82cf2685f83a114e3ad9ff7
kc-clintone <kayseeclintone@gmail.com>
2024-03-19 19:53:12 +0000

    0x0D-SQL_introduction >>> corrections

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,7 @@
 -- displays the average temp by city ordered by temp  ascending)
 
-SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
+SELECT city, AVG(value)
+AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
