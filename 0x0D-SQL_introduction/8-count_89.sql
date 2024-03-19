567a8137d6e2cbb97503fb0a1f412bbee1068147
kc-clintone <kayseeclintone@gmail.com>
2024-03-19 19:46:41 +0000

    0x0D-SQL_introduction >>> corrections

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,6 @@
 -- Displays number of records 
 -- With id = 89 in first_table table of the hbtn_0c_0 database in MySQL server
 
-SELECT COUNT(*) FROM first_table WHERE id=89;
+SELECT COUNT(*)
+FROM first_table
+WHERE id=89;
