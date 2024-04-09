fb27110238ae9ff8c9708f281324ba7dbd00086d
kc-clintone <kayseeclintone@gmail.com>
2024-04-09 22:27:28 +0000

    0x13-javascript_objects_scopes_closures >>> task 11

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const list = require('./100-data.js').list;
+const l = require('./100-data.js').list;
 const n = list.map((k, v) => k * v);
 
-console.log(list);
+console.log(l);
 console.log(n);
