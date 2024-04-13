cdf3f470c97a1b5bfc75f0aaa842716872e23ff3
kc-clintone <kayseeclintone@gmail.com>
2024-04-13 23:58:18 +0000

    0x0F-python-object_relational_mapping >>> task 6

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This script lists all State objects from the
+database `hbtn_0e_6_usa`.
+"""
+
+from sys import argv
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """
+    Accessed the database and get the states
+    """
+
+    dburl = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+
+    ngin = create_engine(dburl)
+    Session = sessionmaker(bind=ngin)
+
+    session = Session()
+
+    for i in session.query(State).order_by(State.id):
+        print('{0}: {1}'.format(i.id, i.name))
