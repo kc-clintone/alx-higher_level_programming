f0566dbb63cd7b3c9c257364841c4f044e38835b
kc-clintone <kayseeclintone@gmail.com>
2024-04-13 23:01:42 +0000

    0x0F-python-object_relational_mapping >>> task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,11 +14,13 @@ if __name__ == '__main__':
     """
     conn = db.connect(host="localhost", port=3306,
            user=argv[1], passwd=argv[2], db=argv[3])
+
     csr = conn.cursor()
 
     csr.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
+    "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
         states.id ASC".format(argv[4]))
+
     resulsts = csr.fetchall()
 
     for item in results:
