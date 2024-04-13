651d0445750b1c6dc22be2ba873e49a732bce180
kc-clintone <kayseeclintone@gmail.com>
2024-04-13 22:30:04 +0000

    0x0F-python-object_relational_mapping >>> task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,10 +14,9 @@ Establish connection to database and retrieve all states
 if __name__ == '__main__':
     sqldb = db.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     dbcsr = sqldb.cursor()
-
-    dbcsr.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' \ORDER BY states.id ASC")
-
+    dbcsr.execute(
+    "SELECT * FROM states WHERE name LIKE BINARY 'N%' \ 
+    ORDER BY states.id ASC")
     results = dbcsr.fetchall()
-
     for item in results:
         print(item)
