d5dbb4ffd7021843c3082ec428e7c997622b8022
kc-clintone <kayseeclintone@gmail.com>
2024-04-13 22:46:22 +0000

    0x0F-python-object_relational_mapping >>> task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-A script that takes in an argument and displays all values
-in the states where `name` matches the argument from the
-database `hbtn_0e_0_usa`.
+A script that takes in an argument and displays all
+values in the states where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
 """
 
 import MySQLdb as db
@@ -16,9 +16,8 @@ if __name__ == '__main__':
            host="localhost", port=3306, user=argv[1],
            passwd=argv[2], db=argv[3])
     csr = conn.cursor()
-    csr.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER
-        BY \ states.id ASC".format(argv[4]))
+    csr.execute("SELECT * FROM states WHERE name LIKE BINARY
+    '{}' ORDER BY \ states.id ASC".format(argv[4]))
     res = db_cursor.fetchall()
     for item in res:
         print(item)
