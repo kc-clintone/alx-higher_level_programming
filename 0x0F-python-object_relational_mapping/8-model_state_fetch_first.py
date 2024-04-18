0ba5d8be21bdb4e45cf413fe30009efcda804df6
kc-clintone <kayseeclintone@gmail.com>
2024-04-18 21:14:31 +0000

    0x0F-python-object_relational_mapping >>> corrections

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This is a script that prints the first State object
+from `hbtn_0e_6_usa` database.
+"""
+
+from sys import argv
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """
+    Access the database and retrieve the state
+    """
+
+    database_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+
+    ngn = create_engine(database_url)
+    sn = sessionmaker(bind=ngn)
+
+    x = sn()
+
+    results = x.query(State).order_by(State.id).first()
+    if results is not None:
+        print('{0}: {1}'.format(results.id, results.name))
+    else:
+        print("Nothing")
