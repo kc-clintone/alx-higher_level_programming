442e1c6fc07410d0c1b6a1b30fb196cda2e344ca
kc-clintone <kayseeclintone@gmail.com>
2024-04-18 21:26:13 +0000

    0x0F-python-object_relational_mapping >>> corrections

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This is a script that lists all State objects that
+contain the character `a` from `hbtn_0e_6_usa` database
+"""
+
+from sys import argv
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """
+    Access the database and retrieve state
+    """
+    database_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+
+    ngn = create_engine(database_url)
+    sn = sessionmaker(bind=ngn)
+
+    x = sn()
+
+    res = x.query(State).filter(State.name.contains('a'))
+    if res is not None:
+        for res in states:
+            print('{0}: {1}'.format(res.id, res.name))
