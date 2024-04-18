ba8e08253df7c4fdbff99c143c9f3d15da6fb02a
kc-clintone <kayseeclintone@gmail.com>
2024-04-18 22:13:00 +0000

    0x0F-python-object_relational_mapping >>> corrections

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -12,14 +12,11 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == '__main__':
     ngn = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-
+                        format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                        pool_pre_ping=True)
     sn = sessionmaker(bind=ngn)
     x = sn()
-
     res = x.query(State).join(City).order_by(City.id).all()
-
     for i in res:
         for j in i.cities:
             print("{}: {} -> {}".format(j.id, j.name, i.name))
