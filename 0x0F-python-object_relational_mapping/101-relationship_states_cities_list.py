94c8d6bec8695e3a4f6d63ee36bc8667506a4488
kc-clintone <kayseeclintone@gmail.com>
2024-04-18 22:23:49 +0000

    0x0F-python-object_relational_mapping >>> corrections

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """
-This script lists all City objects from the
-hbtn_0e_101_usa database
+This script lists all State objects and City objects
+ in the database
 """
-
 import sys
 from relationship_state import Base, State
 from relationship_city import City
@@ -15,9 +14,12 @@ if __name__ == '__main__':
     ngn = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                         format(sys.argv[1], sys.argv[2], sys.argv[3]),
                         pool_pre_ping=True)
+    Base.metadata.create_all(ngn)
+
     sn = sessionmaker(bind=ngn)
     x = sn()
-    res = x.query(State).join(City).order_by(City.id).all()
+    res = x.query(State).outerjoin(City).order_by(State.id, City.id).all()
     for i in res:
+        print("{}: {}".format(i.id, i.name))
         for j in i.cities:
-            print("{}: {} -> {}".format(j.id, j.name, i.name))
+            print("    {}: {}".format(j.id, j.name))
