6e10409bf4402688ddc5bb823ae573d99a4938f6
kc-clintone <kayseeclintone@gmail.com>
2024-04-25 20:05:04 +0000

    0x10-python-network_0 >>> task 0

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#Bash script that takes in a URL, sends a request to that URL
+#and displays the size of the body of the response
+
+# Check if URL is passed
+if [ -z "$1" ]; then
+    echo "URL missng: Usage: $0 URL"
+    exit 1
+fi
+
+# Send request to the URL and get response size
+res=$(curl -s -o /dev/null -w "%{size_download}" "$1")
+
+# Display the response size
+echo "$res"
+
