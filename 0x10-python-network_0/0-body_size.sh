cf57fb3904395b80e10afb19625f390436b4f8d1
kc-clintone <kayseeclintone@gmail.com>
2024-04-25 20:22:17 +0000

    0x10-python-network_0 >>> task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-[ -z "$1" ] && echo "Usage: $0 URL" && exit 1; curl -s -o /dev/null -w "%{size_download}" "$1"
-
+[ -z "$1" ] && echo "Usage: $0 URL" && exit 1;curl -s -o /dev/null -w "%{size_download}" "$1"
