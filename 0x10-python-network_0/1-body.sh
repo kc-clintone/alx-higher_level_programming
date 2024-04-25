76e4ba9a37109dc5cce9938137f66dd84de9f1c4
kc-clintone <kayseeclintone@gmail.com>
2024-04-25 20:34:35 +0000

    0x10-python-network_0 >>> task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -sL "$1"
+[ -z "$1" ] && echo "Usage: $0 URL" && exit 1;curl -s -o /dev/null --write-out "%{http_code}" "$1" | grep -q 200 && curl -s "$1"
+
