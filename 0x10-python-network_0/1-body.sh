62dc988683e7752aedc56886f58da93241589b84
kc-clintone <kayseeclintone@gmail.com>
2024-04-25 20:31:50 +0000

    0x10-python-network_0 >>> task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-[ -z "$1" ] && echo "Usage: $0 URL" && exit 1;curl -s -o /dev/null -w "%{size_download}" "$1"
+curl -sL "$1"
