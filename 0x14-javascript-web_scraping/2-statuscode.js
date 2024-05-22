4dd9263ec1eb4c8ace6f369b989a1e61b2fdbc0e
kc-clintone <kayseeclintone@gmailcom>
2024-05-22 22:46:55 +0000

    0x14-javascript-web_scraping >>> 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const req = require('request');
+const payload = process.argv[2];
+
+request(payload, function (e, res) {
+  if (e) {
+    console.log(e);
+  } else {
+    console.log('code: ' + res.statusCode);
+  }
+});
