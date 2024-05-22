8a3cbd5f34a57010a2b23e58a16c058914992d53
kc-clintone <kayseeclintone@gmailcom>
2024-05-22 23:12:59 +0000

    0x14-javascript-web_scraping >>> 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,33 +2,27 @@
 
 const request = require('request');
 const fs = require('fs');
-
-if (process.argv.length !== 4) {
-    console.error('Usage: node script.js <URL> <filePath>');
-    process.exit(1);
-}
-
 const url = process.argv[2];
 const filePath = process.argv[3];
 
+if (process.argv.length !== 4) {
+    console.error('Error');
+    process.exit(1);
+}
 request(url, (error, response, body) => {
     if (error) {
-        console.error('Error fetching the URL:', error);
+        console.error('Error:', error);
         return;
     }
-
     if (response.statusCode !== 200) {
-        console.error(`Request failed with status code: ${response.statusCode}`);
+        console.error(`Request failed. Status code: ${response.statusCode}`);
         return;
     }
-
     fs.writeFile(filePath, body, 'utf8', (err) => {
         if (err) {
             console.error('Error writing to file:', err);
             return;
         }
-
-        console.log(`Content saved to ${filePath}`);
     });
 });
 
