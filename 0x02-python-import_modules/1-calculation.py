af0e4cf93a763c23b8f254a4993678f8be137daa
kc-clintone <kayseeclintone@gmail.com>
2024-08-06 20:52:27 +0000

    correction

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
-
+    print("{0:d} + {1:d} = {2:d}".format(a, b, add(a, b)))
+    print("{0:d} - {1:d} = {2:d}".format(a, b, sub(a, b)))
+    print("{0:d} * {1:d} = {2:d}".format(a, b, mul(a, b)))
+    print("{0:d} / {1:d} = {2:d}".format(a, b, div(a, b)))
