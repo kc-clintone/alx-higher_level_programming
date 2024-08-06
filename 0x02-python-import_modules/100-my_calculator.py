a6a7112a76adebac2358d2eb275d6b6c665ea8bf
kc-clintone <kayseeclintone@gmail.com>
2024-08-06 21:19:21 +0000

    correction

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,52 +1,31 @@
 #!/usr/bin/python3
-
-def add(a, b):
-    """My addition function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
+import sys
+from calculator_1 import add, sub, mul, div
 
 
-def sub(a, b):
-    """My subtraction function
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-    Args:
-        a: first integer
-        b: second integer
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
 
-    Returns:
-        The return value. a - b
-    """
-    return (a - b)
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    elif operator == '/':
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
+    print(f"{a} {operator} {b} = {result}")
 
-def mul(a, b):
-    """My multiplication function
+if __name__ == "__main__":
+    main()
 
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a * b
-    """
-    return (a * b)
-
-
-def div(a, b):
-    """My division function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a / b
-    """
-    return int(a / b)
