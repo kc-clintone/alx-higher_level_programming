a8b030b023e968116aac51e1df4629a5b4d06ecf
kc-clintone <kayseeclintone@gmail.com>
2024-08-06 20:45:14 +0000

    correction

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+if __name__ == '__main__':
+    from add_0 import add
     a = 1
     b = 2
-    from add_0 import add
-    results = add(a, b)
-    print("{} + {} = {}".format(a, b, results))
-
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
