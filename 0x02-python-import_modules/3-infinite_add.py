7697bfc2b45702546d5a920ede9d89e4de7b5fbe
kc-clintone <kayseeclintone@gmail.com>
2024-08-06 20:55:32 +0000

    correction

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
+import sys
 
-def sum_args(*args):
-    try:
-        total = sum(int(arg) for arg in args)
+if __name__ == '__main__':
+    argv = sys.argv
+    num = len(argv)
+    if num == 1:
+        print(0)
+    else:
+        total = 0
+        for i in argv[1:]:
+            total += int(i)
         print(total)
-    except ValueError:
-        print("Error")
-
-if __name__ == "__main__":
-    import sys
-    sum_args(*sys.argv[1:])
-
