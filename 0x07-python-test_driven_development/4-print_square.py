b527076f73b1e44b1fdbab0dd1a9eaacfb6d74f8
kc-clintone <kayseeclintone@gmail.com>
2024-08-20 10:52:17 +0000

    Corrections

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -2,17 +2,23 @@
 """
 This moduled contains a function that prints the # character
 """
+
+
 def print_square(size):
+    """
+    This function prints a shape using the # charachter
+    Args:
+        Size of the sqaure
+    Return:
+        Nothing
+    """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
 
     if size < 0:
         raise ValueError("size must be >= 0")
-
-    # Check if size is a float and less than 0
     if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
 
-    # Print the square
     for _ in range(size):
         print("#" * size)
